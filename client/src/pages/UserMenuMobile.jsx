fefa67cf9d72f45a0a4e4ef1287a85201b9faae7
Subhamoy Chowdhury <subhamoychowdhury4@gmail.com>
2026-08-09 02:57:07 +0530

    fix mobile view customer support and faq button

--- client/src/pages/UserMenuMobile.jsx
+++ client/src/pages/UserMenuMobile.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserMenu from '../components/UserMenu'
 import { IoClose } from "react-icons/io5";
+import CustomerSupportModal from '../components/CustomerSupportModal';
 
 const UserMenuMobile = () => {
+  const [openSupportModal, setOpenSupportModal] = useState(false)
+
   return (
-    <section className='bg-white h-full w-full py-2'>
-        <button onClick={()=>window.history.back()} className='text-neutral-800 block w-fit ml-auto'>
+    <section className='bg-white min-h-screen w-full py-2'>
+        <button onClick={()=>window.history.back()} className='text-neutral-800 block w-fit ml-auto p-2 cursor-pointer'>
           <IoClose size={25}/>
         </button>
         <div className='container mx-auto px-3 pb-8'>
-           <UserMenu/>
+           <UserMenu openSupport={() => setOpenSupportModal(true)} />
         </div>
+
+        <CustomerSupportModal isOpen={openSupportModal} onClose={() => setOpenSupportModal(false)} />
     </section>
   )
 }
