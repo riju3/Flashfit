2ce3ce674411fb6f86baf304519c4a2cd1308088
Subhamoy Chowdhury <subhamoychowdhury4@gmail.com>
2026-08-09 03:15:33 +0530

    fix useLocation import in Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaPinterest } from "react-icons/fa";
 import { MdEmail, MdPhone, MdLocationOn } from 'react-icons/md';
